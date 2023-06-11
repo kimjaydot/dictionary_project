60236a2b801abfbd9aa8fe3e3e6a60f97483502d
Kimberley Johnson <kimjaydot@gmail.com>
2023-06-11 15:23:53 -0500

    added definition meanings

---
+++ src/Results.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Results(props) {
+  console.log(props.results);
+  if (props.results) {
+    return (
+      <div className="Results">
+        <h2>{props.results.word}</h2>
+        {props.results.meanings.map(function (meaning, index) {
+          return meaning.definitions[0].definition;
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
