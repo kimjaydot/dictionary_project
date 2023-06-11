299d2b356393283d3d86c83a85d7fe0f5b1e58ad
Kimberley Johnson <kimjaydot@gmail.com>
2023-06-11 16:08:55 -0500

    added meaning and result component

--- src/Results.js
+++ src/Results.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Meaning from "./Meaning";
 
 export default function Results(props) {
   console.log(props.results);
@@ -7,7 +8,11 @@ export default function Results(props) {
       <div className="Results">
         <h2>{props.results.word}</h2>
         {props.results.meanings.map(function (meaning, index) {
-          return meaning.definitions[0].definition;
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
         })}
       </div>
     );
