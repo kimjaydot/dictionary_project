ec420ca319dd8724a037e6f9c90f85dd9fcf0015
Kimberley Johnson <kimjaydot@gmail.com>
2023-06-11 13:34:33 -0500

    added search function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import "./Dictionary.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">Hello</header>
+    <div className="container">
+      <div className="App">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="App-footer">
+          <small>Coded By Kimberley Johnson</small>
+        </footer>
+      </div>
     </div>
   );
 }
