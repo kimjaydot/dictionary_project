523afa8f964b34595aa877d2827c370c32fbc911
Kimberley Johnson <kimjaydot@gmail.com>
2023-06-15 17:10:51 -0500

    added pexels api authorization headers

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./Phonetics.css";
 
 export default function Phonetic(props) {
-  console.log(props.phoetic);
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
